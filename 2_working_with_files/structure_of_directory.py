847e901341687f03b1f94fdb4bb448464b7b0dea
Krzysztof Bera <krzysztof.bera9@gmail.com>
2021-10-19 20:34:49 +0200

    class for complex numbers

--- 2_working_with_files/structure_of_directory.py
+++ 2_working_with_files/structure_of_directory.py
@@ -5,12 +5,12 @@ def print_with_tabulation(path, depth):
 	print("\t" * depth + path)
 
 
-def find_files_recursively(start_dir, depth=1):
+def find_files_recursively(start_dir, depth=0):
 	last_element_of_dir = start_dir.split("\\")[-1]
-	print_with_tabulation(last_element_of_dir, depth - 1)
+	print_with_tabulation(last_element_of_dir, depth)
 	for element in os.listdir(start_dir):
 		if not os.path.isdir(os.path.join(start_dir, element)):
-			print_with_tabulation(element, depth)
+			print_with_tabulation(element, depth + 1)
 		else:
 			find_files_recursively(os.path.join(start_dir, element), depth + 1)
 
---
+++ 5_classes/complex_number.py
@@ -0,0 +1,96 @@
+import cmath
+import re
+
+
+class ComplexNumber:
+    """
+    A class representing complex number
+    """
+
+    def __init__(self, real, imag=0.0):
+        self.real = real
+        self.imag = imag
+
+    def __repr__(self):
+        return f"({self.real}{'+' if self.imag >= 0 else '-'}{abs(self.imag)}j)"
+
+    def __add__(self, other):
+        # create interface between my class 'ComplexNumber' and types: 'int', 'float'
+        if isinstance(other, (float, int)):
+            other = ComplexNumber(other)
+        return ComplexNumber(self.real + other.real, self.imag + other.imag)
+
+    def __radd__(self, other):
+        if isinstance(other, (float, int)):
+            other = ComplexNumber(other)
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        if isinstance(other, (float, int)):
+            other = ComplexNumber(other)
+        return ComplexNumber(self.real - other.real, self.imag - other.imag)
+
+    def __rsub__(self, other):
+        if isinstance(other, (float, int)):
+            other = ComplexNumber(other)
+        return self.__sub__(other)
+
+    def __mul__(self, other):
+        if isinstance(other, (float, int)):
+            other = ComplexNumber(other)
+        return ComplexNumber(self.real * other.real - self.imag * other.imag,
+                             self.imag * other.real + self.real * other.real)
+
+    def __truediv__(self, other):
+        if isinstance(other, (float, int)):
+            other = ComplexNumber(other)
+        divisor = (other.real ** 2 + other.imag ** 2)
+        return ComplexNumber((self.real * other.real) - (self.imag * other.imag) / divisor,
+                             (self.imag * other.real) + (self.real * other.imag) / divisor)
+
+    def __abs__(self):
+        return cmath.sqrt(self.real ** 2 - (self.imag ** 2))
+
+    def __neg__(self):
+        return ComplexNumber(-self.real, -self.imag)
+
+    def __eq__(self, other):
+        return self.real == other.real and self.imag == other.imag
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __pos__(self):
+        return self
+
+    @staticmethod
+    def _illegal(operator):
+        print(f"Illegal operation {operator} for complex numbers")
+
+    def __gt__(self, other):
+        self._illegal('>')
+
+    def __ge__(self, other):
+        self._illegal('>=')
+
+    def __lt__(self, other):
+        self._illegal('<')
+
+    def __le__(self):
+        self._illegal('<=')
+
+
+if __name__ == '__main__':
+    # example of usage
+
+    num1 = ComplexNumber(1, 2)
+    num2 = ComplexNumber(3, 4)
+    print(f"Adding numbers {num1} + {num2} = {num1 + num2}")
+    print(f"Subtracting numbers {num1} - {num2} = {num1 - num2}")
+    print(f"Multiplying numbers {num1} * {num2} = {num1 * num2}")
+    print(f"Dividing numbers: {num1} / {num2} = {num1 / num2}")
+    print(f"Absolute value of {num1} = {abs(num1)}")
+    print(f"Negation of {num1} = {-num1}")
+
+    num3 = 12
+    print(f"Adding float to complex number: {num1 + num3}")
